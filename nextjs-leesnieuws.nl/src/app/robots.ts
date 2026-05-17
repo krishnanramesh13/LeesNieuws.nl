cce20886736e7f88e83be5bcc554a07168010f14
krishnanramesh13 <148586743+krishnanramesh13@users.noreply.github.com>
2026-05-17 18:08:55 +0200

    fix: allow Facebook crawler in robots.ts

---
+++ nextjs-leesnieuws.nl/src/app/robots.ts
@@ -0,0 +1,25 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: 'facebookexternalhit',
+        allow: '/',
+      },
+      {
+        userAgent: 'Twitterbot',
+        allow: '/',
+      },
+      {
+        userAgent: 'WhatsApp',
+        allow: '/',
+      },
+    ],
+    sitemap: 'https://nieuwsleren.nl/sitemap.xml',
+  }
+}
